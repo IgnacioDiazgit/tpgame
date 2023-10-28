10afe9a8d2dc81fc3a857c1c9f4573d61bfaf45e
JID89 <joseignaciodiaz89@gmail.com>
2023-10-28 10:54:28 -0300

    ppt casi listo, faltan las imagenes

--- Desktop/tp gameweb/juegos/piedrapapeltijera/js/ppt.js
+++ Desktop/tp gameweb/juegos/piedrapapeltijera/js/ppt.js
@@ -9,7 +9,15 @@ const ppt_player = document.getElementById("ppt_player");
 const ppt_bot = document.getElementById("ppt_bot");
 const divpiedrapapeltijera = document.getElementById("divpiedrapapeltijera");
 const cerrar_juego = document.getElementById("cerrar_juego");
-const reset = document.getElementById("reset");
+const resetppt = document.getElementById("resetppt");
+const divgameppt= document.getElementById("divgame");
+const fondoppt = document.getElementById("fondoppt");
+const mensajeppt = document.getElementById("mensajeppt");
+
+const imgppt_player = document.getElementById("imgppt_player");
+const imgppt_pc = document.getElementById("imgppt_pc");
+
+
 
 
 
@@ -29,16 +37,46 @@ const reset = document.getElementById("reset");
 
 
 const ppt = ["piedra","papel","tijera"];
-const pptpc = [];
+let ppt_pc = "";
+let ppt_playerr = "";
+
 
 
 
 function randompc(){
     let randompick = Math.floor(Math.random()*ppt.length);
-    pptpc.push(ppt[randompick]);
+    ppt_pc = ppt[randompick];
 
 }
+function juego_ppt(){
+    randompc();
+    const mensajeppt = document.getElementById("mensajeppt");
 
+
+    if ( ppt_playerr === ppt_pc) {
+        mensajeppt.textContent = "Ambos elejimos "+ ppt_pc+" esto es un empate. ";
+
+    }else if (ppt_playerr === "piedra" && ppt_pc === "tijera" ){
+        mensajeppt.textContent = "Sacaste  piedra y yo tijeras. Ganaste.";
+    
+    }else if (ppt_playerr === "piedra" && ppt_pc === "papel"){
+        mensajeppt.textContent = "Sacaste piedra y yo papel. Gane.";
+
+    }else if (ppt_playerr === "papel" && ppt_pc === "tijera"){
+        mensajeppt.textContent = "Sacaste papel y yo tijeras. Gane.";
+
+    }else if (ppt_playerr === "papel" && ppt_pc === "piedra"){
+        mensajeppt.textContent = "Sacaste papel y yo piedra. Ganaste.";
+
+    }else if (ppt_playerr === "tijera" && ppt_pc === "piedra"){
+        mensajeppt.textContent = "Sacaste tijeras y yo piedra. Gane.";
+
+    }else if (ppt_playerr === "tijera" && ppt_pc === "papel"){
+        mensajeppt.textContent = "Sacaste tijeras y yo papel. Ganaste.";
+
+    }
+
+}
    
            /* botonera */
 
@@ -59,9 +97,14 @@ botonstarppt.addEventListener("click",function(){
     cerrar_juego.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
     cerrar_juego.style.opacity = "1";
     cerrar_juego.style.cursor = "pointer";
-    reset.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
-    reset.style.cursor = "pointer";
-    reset.style.opacity = "1";
+    resetppt.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
+    resetppt.style.cursor = "pointer";
+    resetppt.style.opacity = "1";
+    fondoppt.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
+    fondoppt.style.opacity ="1";
+
+   
+    animacion_inicio();
 
 
 }) 
@@ -76,7 +119,62 @@ cerrar_juego.addEventListener("click",function(){
     divpiedrapapeltijera.style.opacity = "1";
     cerrar_juego.style.opacity = "0";
     cerrar_juego.style.cursor = "default";
-    reset.style.opacity = "0";
-    reset.style.cursor = "default";
+    resetppt.style.opacity = "0";
+    resetppt.style.cursor = "default";
+    fondoppt.style.opacity ="0";
+    ppt_playerr = "";
+    ppt_pc = "";
+    mensajeppt.textContent = "";
+    mensajeppt.style.opacity = "0";
+
 
 })
+resetppt.addEventListener("click",function(){
+    ppt_playerr = "";
+    ppt_pc = "";
+    mensajeppt.textContent = "";
+    mensajeppt.style.opacity = "0";
+})
+
+boton_papel.addEventListener("click",function(){
+    ppt_playerr = "papel";
+    imgvsppt.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
+    imgvsppt.style.opacity = "1";
+    mensajeppt.style.transition = "opacity 1s ease-in-out,transform 0.3s ease";
+    mensajeppt.style.opacity = "0.8";
+    juego_ppt();
+})
+boton_piedra.addEventListener("click",function(){
+    ppt_playerr = "piedra";
+    imgvsppt.style.transition = "opacity 1s ease-in-out, transform 0.3ease";
+    imgvsppt.style.opacity ="1";
+    mensajeppt.style.transition = "opacity 1s ease-in-out, transform 0.3ease";
+    mensajeppt.style.opacity = "1";
+    juego_ppt();
+})
+boton_tijera.addEventListener("click",function(){
+    ppt_playerr = "tijera";
+    imgvsppt.style.transition = "opacity 1s ease-in-out, transform 0.3ease";
+    imgvsppt.style.opacity ="1";
+    mensajeppt.style.transition = "opacity 1s ease-in-out, transform 0.3ease";
+    mensajeppt.style.opacity = "1";
+    juego_ppt();
+})
+
+
+
+                                         /* animaciones */ 
+
+function animacion_inicio(){
+    boton_piedra.style.animationName = "caida_piedra";
+    boton_piedra.style.animationDuration = "2.4s";
+    boton_papel.style.animationName = "caida_papel";
+    boton_papel.style.animationDuration = "2.4s";
+    boton_tijera.style.animationName ="caida_tijera";
+    boton_tijera.style.animationDuration = "2.4s";
+    resetppt.style.animationName = "caida_piedra";
+    resetppt.style.animationDuration = "2.8s";
+    cerrar_juego.style.animationName = "caida_piedra";
+    cerrar_juego.style.animationDuration = "2.4s";
+
+}
